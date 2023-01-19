911089708fd454ce1fb8e610edac3ef42c5ff83f
adler <agapay.adril@gmail.com>
2023-01-19 12:04:04 +0800

    log-in parameters update

--- backend/tourist_manager/accounts/serializers.py
+++ backend/tourist_manager/accounts/serializers.py
@@ -5,16 +5,17 @@ from django.contrib.auth import authenticate
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username')
+        #fields = ('id', 'username')
+        fields = '__all__'
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password')
+        fields = ('id', 'username', 'first_name', 'last_name', 'password', 'is_staff')
         extra_kwargs = {'password':{'write_only': True}}
 
     def create(self, validated_data):
-        user = User(username=validated_data['username']) 
+        user = User(username=validated_data['username'], first_name=validated_data['first_name'], last_name=validated_data['last_name']) 
         user.set_password(validated_data['password'])
         user.save()
         return user
